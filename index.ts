faa99cccce09450dde392f651f868cd5d750d228
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-14 16:46:06 +0300

    fix warn -> info

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ console.log('clock rock lala yaya')
 
 export const load = (jumpFm) => {
     const update = () => {
-        jumpFm.statusBar.warn('clock2',
+        jumpFm.statusBar.info('clock2',
             moment(Date.now()).format(
                 jumpFm.settings.getStr('dateFormat', 'MM/DD/YYYY hh:mm:ss')
             )
