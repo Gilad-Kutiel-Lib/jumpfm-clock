b47cb066f47bb3f0ef8b6132717b8188bfd28e72
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-15 16:32:01 +0300

    fixed

--- index.ts
+++ index.ts
@@ -3,9 +3,7 @@ import { JumpFm } from 'jumpfm-api'
 import * as moment from 'moment'
 
 export const load = (jumpFm: JumpFm) => {
-    console.log('clock')
     const update = () => {
-        console.log('clock')
         const m = moment(Date.now())
         jumpFm.statusBar.info('clock', {
             txt: m.format(
