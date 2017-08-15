f4b192783952b1976f9b12ee001d3b9c17d7bb11
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-15 16:09:42 +0300

    fixed time format

--- index.ts
+++ index.ts
@@ -3,11 +3,13 @@ import { JumpFm } from 'jumpfm-api'
 import * as moment from 'moment'
 
 export const load = (jumpFm: JumpFm) => {
+    console.log('clock')
     const update = () => {
+        console.log('clock')
         const m = moment(Date.now())
         jumpFm.statusBar.info('clock', {
             txt: m.format(
-                jumpFm.settings.getStr('timeFormat', 'hh:mm:ss')
+                jumpFm.settings.getStr('timeFormat', 'HH:mm:ss')
             ),
             dataTitle: m.format(
                 jumpFm.settings.getStr('dateFormat', 'MM/DD/YYYY ')
